0fd33e715eddc9858a8c22eb5c62a8cbac6e6612
Santiago Basulto <santiago.basulto@gmail.com>
2018-03-28 20:03:30 -0300

    Solution

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,39 @@
 import requests
 
+
+INSERT_GIST_QUERY = """INSERT INTO gists (
+    "github_id", "html_url", "git_pull_url",
+    "git_push_url", "commits_url",
+    "forks_url", "public", "created_at",
+    "updated_at", "comments", "comments_url"
+) VALUES (
+    :github_id, :html_url, :git_pull_url,
+    :git_push_url, :commits_url, :forks_url,
+    :public, :created_at, :updated_at,
+    :comments, :comments_url
+);"""
+URL_TEMPLATE = 'https://api.github.com/users/{username}/gists'
+
+
 def import_gists_to_database(db, username, commit=True):
-    pass
+    resp = requests.get(URL_TEMPLATE.format(username=username))
+    resp.raise_for_status()
+
+    gists_data = resp.json()
+    for gist in gists_data:
+        params = {
+            "github_id": gist['id'],
+            "html_url": gist['html_url'],
+            "git_pull_url": gist['git_pull_url'],
+            "git_push_url": gist['git_push_url'],
+            "commits_url": gist['commits_url'],
+            "forks_url": gist['forks_url'],
+            "public": gist['public'],
+            "created_at": gist['created_at'],
+            "updated_at": gist['updated_at'],
+            "comments": gist['comments'],
+            "comments_url": gist['comments_url'],
+        }
+        db.execute(INSERT_GIST_QUERY, params)
+        if commit:
+            db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,62 @@
 from .models import Gist
 
+# <REMOVE ME - FROM HERE
+DATETIME_PREFIXES = ('created_at', 'updated_at')
+
+
+def is_datetime_param(param):
+    for prefix in DATETIME_PREFIXES:
+        if param.startswith(prefix):
+            return True
+    return False
+
+
+def get_operator(comparison):
+    return {
+        'lt': '<',
+        'lte': '<=',
+        'gt': '>',
+        'gte': '>=',
+    }[comparison]
+
+
+def build_query(**kwargs):
+    # This is UGLY as fuck. Please remove.
+    query = 'SELECT * FROM gists'
+    values = {}
+    if kwargs:
+        filters = []
+        for param, value in kwargs.items():
+            if is_datetime_param(param):
+                if '__' in param:
+                    attribute, comparison = param.split('__')
+                    operator = get_operator(comparison)
+                    filters.append(
+                        'datetime(%s) %s datetime(:%s)' % (
+                            attribute, operator, param))
+                else:
+                    attribute = param
+                    filters.append(
+                        'datetime(%s) == datetime(:%s)' % (
+                            attribute, param))
+                values[param] = value
+            else:
+                filters.append(
+                    '%s = :%s' % (
+                        param, param))
+                values[param] = value
+
+        query += ' WHERE '
+        query += ' AND '.join(filters)
+
+    return query, values
+
+# REMOVE ME - TO HERE />
+
 def search_gists(db_connection, **kwargs):
-    pass
+    query, params = build_query(**kwargs)
+    cursor = db_connection.execute(query, params)
+    results = []
+    for gist in cursor:
+        results.append(Gist(gist))
+    return results
