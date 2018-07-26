02931bc17e97807b35017b17ce88279ecbaa3da8
Zona <bryson.gilreath@gmail.com>
2018-07-26 19:51:03 +0000

    cleaned up importer even more

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -10,19 +10,6 @@ def import_gists_to_database(db, username, commit=True):
         
     gists = response.json()
     
-    # gists_for_db = ({
-    #     "github_id": gist['id'],
-    #     "html_url": gist['html_url'],
-    #     'commits_url': gist['commits_url'],
-    #     "git_pull_url": gist['git_pull_url'],
-    #     "git_push_url": gist['git_push_url'],
-    #     "forks_url": gist['forks_url'],
-    #     "public": int(gist['public']),
-    #     "created_at": gist["created_at"],
-    #     "updated_at": gist["updated_at"],
-    #     "comments": gist["comments"],
-    #     "comments_url": gist["comments_url"]} for gist in gists)
-    
     gists_for_db = ((
                 gist['id'],
                 gist['html_url'],
@@ -39,30 +26,6 @@ def import_gists_to_database(db, username, commit=True):
     curs = db.cursor()
     
     for gist in gists_for_db:
-        
-        # '''create paired iters to ensure that
-        # keys and values are in the same order'''
-        # pairs = gist.items()
-        # keys = tuple(pair[0] for pair in pairs)
-        # values = tuple(pair[1] for pair in pairs)
-        # insert_string = '''
-        #         INSERT INTO gists
-        #             {keys}
-        #         VALUES
-        #             {values}
-        #     '''.format(keys=keys, values=values)
-        # 
-        # curs.execute(insert_string)
-        
-        # insert_string = '''
-        #         INSERT INTO gists
-        #             (github_id, html_url, git_pull_url, git_push_url,
-        #             commits_url, forks_url, public, created_at, updated_at,
-        #             comments, comments_url)
-        #         VALUES
-        #             {gist}
-        #     '''.format(gist=gist)
-        # curs.execute(insert_string)
         
         insert_string = '''
                 INSERT INTO gists
